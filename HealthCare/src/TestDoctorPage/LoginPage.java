0885fda6a800bab7167688cff37726a9c013e4c3
Tarique <Tarique@Tarique-PC>
2017-05-15 16:36:06 +0530

    Initial CheckIn.

---
+++ HealthCare/src/TestDoctorPage/LoginPage.java
@@ -0,0 +1,13 @@
+package TestDoctorPage;
+
+public class LoginPage {
+	
+	public static String validLogin(String a,String b)
+	{
+		System.out.println("name is: "+a);
+		return "abc";
+		
+	}
+	
+
+}
---
+++ HealthCare/src/TestDoctorPage/PageObjectLogin.java
@@ -0,0 +1,11 @@
+package TestDoctorPage;
+
+public class PageObjectLogin {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Tha prgram is started..");
+		
+	}
+
+}
