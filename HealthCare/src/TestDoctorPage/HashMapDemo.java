6657ffdc182d8266e07ced2036bb3bd52fd7782d
Tarique <Tarique@Tarique-PC>
2017-05-21 10:34:58 +0530

    New Class added.

---
+++ HealthCare/src/TestDoctorPage/HashMapDemo.java
@@ -0,0 +1,30 @@
+package TestDoctorPage;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("The hash map..");
+		
+		//LinkedHashMap h = new LinkedHashMap();
+		
+		HashMap h = new HashMap();
+		
+		h.put("Chiranjivi", 7);
+		h.put("Balaih", 6);
+		h.put("Venkatesh", 2);
+		h.put("Nagarjuna", 8);
+		
+		System.out.println(h);
+		
+		
+
+		
+		
+	}
+
+}
